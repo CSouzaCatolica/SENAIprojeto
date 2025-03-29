23296b776b72afab0266ea76ccac643ff10bce6e
GABRIEL CHIARELLI DE SOUZA <gabriel_souza61@estudante.sesisenai.org.br>
2025-03-29 16:00:20 -0300

    for

--- app_home/views.py
+++ app_home/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_home/pages/index.html')
+    teste_arr = [
+        {
+            'nome': 'Python',
+            'descricao': 'Linguagem de programação de alto nível'
+        },
+        {
+            'nome': 'Django',
+            'descricao': 'Framework para desenvolvimento web'
+        }
+    ]
+
+    return render(request, 'app_home/pages/index.html', context={'teste': teste_arr})
